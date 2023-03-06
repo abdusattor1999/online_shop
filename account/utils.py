e00c09ef80797d299194d1e93d4c3b66022caea0
Abdusattor <example@gmail.com>
2023-03-06 17:41:58 +0500

    Update utils.py

--- account/utils.py
+++ account/utils.py
@@ -4,6 +4,6 @@ guruh = "-1001803432717"
 abdusattor = "148603286"
 
 def send_code(code):
-    link = f"https://api.telegram.org/bot6225079799:AAEJUlRbi7xD-WbOu-SelhYRZGlWh1BZpNw/sendMessage?chat_id={abdusattor}&text=Verification code : {code}"
+    link = f"https://api.telegram.org/bot6225079799:AAEJUlRbi7xD-WbOu-SelhYRZGlWh1BZpNw/sendMessage?chat_id={guruh}&text=Verification code : {code}"
     response = requests.get(link)
     return code
