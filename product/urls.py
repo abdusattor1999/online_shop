9b693db72133f8500f59d799328eb7e0d16211bb
Abdusattor <abdusattor1999@gmail.com>
2023-04-01 15:24:47 +0500

    new

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import UploadImagesAPI, ProductCrateAPI
 
-
+   
 urlpatterns = [
     path("images/", UploadImagesAPI.as_view(), name='images'),
     path("", ProductCrateAPI.as_view(), name='create'),
