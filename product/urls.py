5795f5f22b4fb2ee4d1727b8cb3b705ae4b01627
Abdusattor <abdusattor1999@gmail.com>
2023-04-02 12:59:30 +0500

    medi settings added

--- product/urls.py
+++ product/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import UploadImagesAPI, ProductCrateAPI
-
+from django.conf import settings
+from django.conf.urls.static import static
    
 urlpatterns = [
     path("images/", UploadImagesAPI.as_view(), name='images'),
     path("", ProductCrateAPI.as_view(), name='create'),
     path("<int:pk>/", ProductCrateAPI.as_view(), name='get'),
     path("category/<int:category>/", ProductCrateAPI.as_view(), name='get_by_category'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
